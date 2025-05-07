b52427f1874ac4293b20ce9971c93571d0ab5591
Juan Esteban Barrios Tovar <jebarriost@eafit.edu.co>
2025-05-07 17:45:58 -0500

    Estructura final

--- data.py
+++ data.py
@@ -6,7 +6,7 @@ pokeneas = [
         "nombre": "PokeneaRoja",
         "altura": 1.2,
         "habilidad": "Fuego eterno",
-        "imagen": "https://i.postimg.cc/s2RcpcQQ/pokenea1.png",
+        "imagen": "https://TU_BUCKET.s3.amazonaws.com/pokenea1.png",
         "frase_filosofica": "El fuego que ilumina también puede consumir."
     },
     {
@@ -14,12 +14,76 @@ pokeneas = [
         "nombre": "PokeneaVerde",
         "altura": 0.8,
         "habilidad": "Regeneración",
-        "imagen": "https://i.postimg.cc/P5qL0vg3/pokenea2.jpg",
+        "imagen": "https://TU_BUCKET.s3.amazonaws.com/pokenea2.png",
         "frase_filosofica": "Cada hoja caída es una lección."
+    },
+    {
+        "id": 3,
+        "nombre": "PokeneaAzul",
+        "altura": 1.0,
+        "habilidad": "Agua pura",
+        "imagen": "https://TU_BUCKET.s3.amazonaws.com/pokenea3.png",
+        "frase_filosofica": "La calma del agua refleja la profundidad del alma."
+    },
+    {
+        "id": 4,
+        "nombre": "PokeneaAmarillo",
+        "altura": 1.5,
+        "habilidad": "Electricidad estática",
+        "imagen": "https://TU_BUCKET.s3.amazonaws.com/pokenea4.png",
+        "frase_filosofica": "La energía es la chispa de la vida."
+    },
+    {
+        "id": 5,
+        "nombre": "PokeneaNegro",
+        "altura": 1.3,
+        "habilidad": "Sombra eterna",
+        "imagen": "https://TU_BUCKET.s3.amazonaws.com/pokenea5.png",
+        "frase_filosofica": "En la oscuridad, la luz brilla más intensamente."
+    },
+    {
+        "id": 6,
+        "nombre": "PokeneaBlanco",
+        "altura": 0.9,
+        "habilidad": "Nieve eterna",
+        "imagen": "https://TU_BUCKET.s3.amazonaws.com/pokenea6.png",
+        "frase_filosofica": "La pureza de la nieve es un reflejo del alma."
+    },
+    {
+        "id": 7,
+        "nombre": "PokeneaRosa",
+        "altura": 1.1,
+        "habilidad": "Amor eterno",
+        "imagen": "https://TU_BUCKET.s3.amazonaws.com/pokenea7.png",
+        "frase_filosofica": "El amor es la fuerza más poderosa del universo."
+    },
+    {
+        "id": 8,
+        "nombre": "PokeneaMorado",
+        "altura": 1.4,
+        "habilidad": "Misterio eterno",
+        "imagen": "https://TU_BUCKET.s3.amazonaws.com/pokenea8.png",
+        "frase_filosofica": "El misterio es el alma de la curiosidad."
+    },
+    {
+        "id": 9,
+        "nombre": "PokeneaCyan",
+        "altura": 1.6,
+        "habilidad": "Agua helada",
+        "imagen": "https://TU_BUCKET.s3.amazonaws.com/pokenea9.png",
+        "frase_filosofica": "El agua fría es un refugio para el alma."
+    },
+    {
+        "id": 10,
+        "nombre": "PokeneaNaranja",
+        "altura": 1.7,
+        "habilidad": "Fuego ardiente",
+        "imagen": "https://TU_BUCKET.s3.amazonaws.com/pokenea10.png",
+        "frase_filosofica": "El fuego ardiente es la pasión del alma."
     }
+    
+    
 ]
-
-
 
 def obtener_pokenea_aleatoria():
     return random.choice(pokeneas)
