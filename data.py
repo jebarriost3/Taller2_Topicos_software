5c1370ee00340616b7637fb27c691830e6174120
Juan Esteban Barrios Tovar <jebarriost@eafit.edu.co>
2025-05-07 17:21:33 -0500

    def

--- data.py
+++ data.py
@@ -6,7 +6,7 @@ pokeneas = [
         "nombre": "PokeneaRoja",
         "altura": 1.2,
         "habilidad": "Fuego eterno",
-        "imagen": "https://pokenea-imagenes-jebarriost.s3.us-east-1.amazonaws.com/pokenea1.png",
+        "imagen": "https://i.postimg.cc/s2RcpcQQ/pokenea1.png",
         "frase_filosofica": "El fuego que ilumina también puede consumir."
     },
     {
@@ -14,10 +14,11 @@ pokeneas = [
         "nombre": "PokeneaVerde",
         "altura": 0.8,
         "habilidad": "Regeneración",
-        "imagen": "https://pokenea-imagenes-jebarriost.s3.us-east-1.amazonaws.com/pokenea2.png",
+        "imagen": "https://i.postimg.cc/P5qL0vg3/pokenea2.jpg",
         "frase_filosofica": "Cada hoja caída es una lección."
     }
 ]
+
 
 
 def obtener_pokenea_aleatoria():
